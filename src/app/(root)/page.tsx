b08ff81f407212c8b25c269a261e80f5433b7d11
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2023-10-25 11:13:22 +0300

    text: base url

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -2,22 +2,22 @@ import { About } from "@/components/sections/about";
 import { Contact } from "@/components/sections/contact";
 import { Hero } from "@/components/sections/hero";
 import axios from "axios";
+import { headers, cookies } from "next/headers";
 
 const Home = async () => {
+  const h = { cookie: cookies().toString() };
   const base =
     process.env.NODE_ENV === "production"
-      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+      ? `https://${headers().get("Host")}`
       : process.env.NEXT_PUBLIC_SITE_URL;
 
-  const res = await axios.get(`${base}/api/skills`);
-
-  
+  const { data } = await axios.get(`${base}/api/skills`, { headers: h });
 
   return (
     <main className="w-full flex flex-col relative">
       <Hero />
       <About />
-      <Contact data={res}/>
+      <Contact data={data} />
     </main>
   );
 };
