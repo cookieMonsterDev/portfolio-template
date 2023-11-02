651c916e394f0b9a39b1d28397cd440c4f9feec2
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2023-11-02 09:10:16 +0200

    feat: add posts page & post page

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,4 @@
-import { type ClassValue, clsx } from "clsx";
-import { NextResponse } from "next/server";
+import { type ClassValue, clsx } from "clsx";;
 import { twMerge } from "tailwind-merge";
 import { ZodSchema } from "zod";
 
