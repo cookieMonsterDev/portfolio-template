57586555c2b31eea50e3c37c2d09dafcfe943c45
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2023-12-15 11:32:26 +0200

    feat: add loading skeletons

--- src/app/(root)/projects/components/project-list.tsx
+++ src/app/(root)/projects/components/project-list.tsx
@@ -30,8 +30,6 @@ const ProjectList = async ({
     headers: h,
   });
 
-  await new Promise((res) => setTimeout(res, 3000));
-
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
       {projects.map((e: Project) => (
